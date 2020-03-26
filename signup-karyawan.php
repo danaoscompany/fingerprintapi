db297807c9b04dcca833a9b45a0efeea2e04c65b
Dana Prakoso Eka Saputra <danaoscompany@gmail.com>
2020-03-27 05:28:53 +0700

    a

--- signup-karyawan.php
+++ signup-karyawan.php
@@ -2,12 +2,12 @@
 
 include 'db.php';
 
-$userID = intval($_POST['user_id']);
+$admimID = intval($_POST['admin_id']);
 $name = $_POST['name'];
 $address = $_POST['address'];
 $birthplace = $_POST['birth_place'];
 $birthday = $_POST['birthday'];
 move_uploaded_file($_FILES['file']['tmp_name'], 'pictures/' . $_FILES['file']['name']);
-$c->query("INSERT INTO karyawan (user_id, name, address, birth_place, birthday, photo) VALUES (" . $userID . ", '" . $name . "', '" . $address . "', '" . $birthplace . "', '" . $birthday . "', 'pictures/" . $_FILES['file']['name'] . "')");
+$c->query("INSERT INTO karyawan (admin_id, name, address, birth_place, birthday, photo) VALUES (" . $adminID . ", '" . $name . "', '" . $address . "', '" . $birthplace . "', '" . $birthday . "', 'pictures/" . $_FILES['file']['name'] . "')");
 $id = mysqli_insert_id($c);
 echo json_encode($c->query("SELECT * FROM karyawan WHERE id=" . $id)->fetch_assoc());
