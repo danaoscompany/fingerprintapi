dd5104b9fb61fe59e966a4115014b8157e89ecd6
Dana Prakoso Eka Saputra <danaoscompany@gmail.com>
2020-03-27 09:58:33 +0700

    a

--- update-face-token.php
+++ update-face-token.php
@@ -4,4 +4,4 @@ include 'db.php';
 
 $userID = intval($_POST['user_id']);
 $faceToken = $_POST['face_token'];
-$c->query("UPDATE karyawan SET face_token='" . $faceToken . "' WHERE user_id=" . $userID);
+$c->query("UPDATE karyawan SET face_token='" . $faceToken . "' WHERE id=" . $userID);
